3db16bec85483bf094569c46955cff78e7fe9134
ngajade <phnga2405@gmail.com>
2021-10-25 18:54:45 +0700

    Á

--- MenuThumbStackAnimation-main/src/js/index.js
+++
@@ -1,8 +0,0 @@
-import { preloadImages } from './utils';
-import { Menu } from './menu';
-
-// preload images then remove loader (loading class) 
-preloadImages('.stack__img').then(() => document.body.classList.remove('loading'));
-
-// initialize Menu
-new Menu(document.querySelector('.menu'));
